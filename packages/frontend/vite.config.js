45d180febf83d22ff2020fd55867add64a1b667a
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-07-25 22:19:19 +0300

    fix: add lerna bootstrpa

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -4,5 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()]
+  plugins: [
+    reactRefresh(),
+    tsconfigPaths()
+  ]
 })
