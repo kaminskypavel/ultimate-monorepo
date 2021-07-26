28d005d5cbe907a578a9099dabadabd27d57a01b
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-07-26 20:03:19 +0300

    fix(frontend): do not purge tailwind shared component on build

--- packages/ui/frontend/tailwind.config.js
+++ packages/ui/frontend/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  // ! todo : explore why everything is being purged
-  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: false,
+  purge: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './../components/src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
