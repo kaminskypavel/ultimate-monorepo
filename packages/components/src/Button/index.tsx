b544c1c0427fbf4b034a2f798e66c2d743be99aa
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-07-26 16:31:55 +0300

    fix(components): remove dead css and use tailwind insted

--- packages/components/src/Button/index.tsx
+++ packages/components/src/Button/index.tsx
@@ -4,6 +4,10 @@ type Props = {
   label: string;
 };
 
-const Button = ({label}: Props) => <button>{label}</button>;
+const Button = ({label}: Props) => (
+  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
+    {label}
+  </button>
+);
 
 export default Button;
